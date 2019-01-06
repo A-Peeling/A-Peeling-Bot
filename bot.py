4c8aba4f892fc4538656ef89ba92c4e96a53bc2c
stophman1 <11683358+stophman1@users.noreply.github.com>
2019-01-05 22:58:36 -0600

    Added the ability for the ;game command to save.

--- bot.py
+++ bot.py
@@ -14,12 +14,19 @@ ownerid = f.read()
 f.seek(0)
 f.close()
 
+f = open('lastgame.txt', 'r')
+argtxt = f.read()
+f.seek(0)
+f.close()
+
+
 p = ";"
 client = commands.Bot(command_prefix=p)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))    
+    await client.change_presence(game=discord.Game(name=argtxt))
     
 @client.command(pass_context=True, brief='Responds \'Poing!\'.', description='Responds \'Poing!\', This command is used for testing the bot.')
 async def ping(ctx):
@@ -28,7 +35,11 @@ async def ping(ctx):
 @client.command(pass_context=True, brief='[Owner Only] Changes the current game.', description='[Owner Only] Changes the current game.')
 async def game(ctx, arg):
         if ctx.message.author.id == ownerid:
-         await client.change_presence(game=discord.Game(name=arg))
          await client.say("Setting game to " + arg)
-
+         print('Game set to '+ arg)
+         with open('lastgame.txt', 'w') as file:
+          file.write(arg)
+         file.close()
+         await client.change_presence(game=discord.Game(name=arg))
+         
 client.run(token)
