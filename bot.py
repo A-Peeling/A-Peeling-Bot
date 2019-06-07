225112f782f534a79f3155b76834d55aab6bac76
stophman1 <11683358+stophman1@users.noreply.github.com>
2019-06-07 14:40:56 -0500

    Updated for new discord.py and python
    I hate these syntax rules

--- bot.py
+++ bot.py
@@ -26,31 +26,35 @@ client = commands.Bot(command_prefix=p)
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))    
-    await client.change_presence(game=discord.Game(name=argtxt))
-    
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(name=argtxt))
+
+
 @client.command(pass_context=True, brief='Responds \'Pong!\'.', description='Responds \'Pong!\', This command is used for testing the bot.')
 async def ping(ctx):
-        await client.say("Pong!")
-        
+    await ctx.send("Pong!")
+
+
 @client.command(pass_context=True, brief='[Owner Only] Changes the current game.', description='[Owner Only] Changes the current game.')
-async def game(ctx, arg):
-        if ctx.message.author.id == ownerid:
-         await client.say("Setting game to " + arg)
-         print('Game set to '+ arg)
-         with open('lastgame.txt', 'w') as file:
-          file.write(arg)
-         file.close()
-         await client.change_presence(game=discord.Game(name=arg))
+async def game(ctx, *, arg: str):
+    if ctx.message.author.id == ownerid:
+        await ctx.send("Setting game to " + arg)
+        print('Game set to '+ arg)
+    with open('lastgame.txt', 'w') as file:
+        file.write(arg)
+        file.close()
+    await  client.change_presence(activity=discord.Game(name=arg))
+
 
 @client.command()
-async def joined(member : discord.Member):
+async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
-    await client.say('{0.name} joined this server on {0.joined_at}'.format(member))
-     
+    await ctx.send('{0.name} joined this server on {0.joined_at}'.format(member))
+
+
 @client.command(pass_context=True, brief='Work.')
 async def work(ctx):
-        await client.say("Yes you can work on the bot https://github.com/A-Peeling/A-Peeling-Bot")
+    await ctx.send("Yes you can work on the bot https://github.com/A-Peeling/A-Peeling-Bot")
 
 
 if __name__ == "__main__":
--- fun.py
+++ fun.py
@@ -1,50 +1,55 @@
 from discord.ext import commands
 import random
+import asyncio
 
-class Fun():
+class Fun(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
-        
-        
+
     @commands.command(pass_context=True, brief='It\'s just a 8ball.')
     async def magicball(self, ctx, *arg):
         if not arg:
-            await self.bot.say('You need to supply a question')
+            await ctx.send('You need to supply a question')
         else:
          ball8 = random.choice(['It is certain','As i see it, yes', 'Dont count on it', 'Without a doubt', 'Definitely', 'Very doubtful', 'Outlook not so good', 'My sources say no', 'My reply is no', 'Most likely', 'You may rely on it', 'Ask again later'])
-         await self.bot.say(ball8)
+         await ctx.send(ball8)
          
     @commands.command(pass_context=True, brief='Guessing Game', description='Guess the Correct Number to win.')
     async def guess(self, ctx):
-    
-        await self.bot.say('Guess a number between 1 to 10')
+            await ctx.send('Guess a number between 1 and 10.')
 
-        def guess_check(m):
-            return m.content.isdigit()
+            def guess_check(m):
+                return m.author == ctx.author and m.content.isdigit()
 
-        guess = await self.bot.wait_for_message(timeout=5.0, author=ctx.message.author, check=guess_check)
-        answer = random.randint(1, 10)
-        if guess is None:
-            fmt = 'Sorry, you took too long. It was {}.'
-            await self.bot.say(fmt.format(answer))
-            return
-        if int(guess.content) == answer:
-            await self.bot.say('You are right!')
-        else:
-            await self.bot.say('Sorry. It is actually {}.'.format(answer))
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.bot.wait_for('message', check=guess_check, timeout=5.0)
+            except asyncio.TimeoutError:
+                fmt = 'Sorry, you took too long. It was {}.'
+                await ctx.send(fmt.format(answer))
+                return
+            if int(guess.content) == answer:
+                await ctx.send('You are right!')
+            else:
+                await ctx.send('Sorry. It is actually {}.'.format(answer))
 
     @commands.command()
-    async def roll(self, dice : str):
+    async def roll(self, ctx, dice : str):
         """Rolls a dice in NdN format."""
         try:
             rolls, limit = map(int, dice.split('d'))
         except Exception:
-            await self.bot.say('Format has to be in NdN!')
+            await ctx.send('Format has to be in NDN. For example 1d20 rolls 1 20 sided dice.')
             return
 
         result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-        await self.bot.say(result)    
-            
+        await ctx.send(result)
+
+    @roll.error
+    async def define_error(self, ctx, error):
+        await ctx.send('Format has to be in NDN. For example 1d20 rolls 1 20 sided dice.')
+
 def setup(bot):
     bot.add_cog(Fun(bot))
