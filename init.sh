dd05d5ef47582dc61e1d07b3aac7d606b82a6ee8
reitraced <with0utyou@protonmail.com>
2020-11-14 00:04:17 -0500

    small fix to init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@ echo "creating virtualenv"
 python3 -m venv ./venv
 source ./venv/bin/activate
 echo "installing dependencies..."
-python3 -m pip -U install discord.py requests
+python3 -m pip install discord.py requests
 echo "done! you can now run the bot."
+echo "don't forget to put your bot token in token.txt"
